d9632efc951c93aeb80261cdf1b0433d771f3a87
Dave Wasmer <davewasmer@gmail.com>
2017-03-06 23:03:45 -0700

    do not use static keyword since we are not transpiling

--- blueprints/denali-typescript/index.js
+++ blueprints/denali-typescript/index.js
@@ -1,8 +1,8 @@
 const { Blueprint } = require('denali-cli');
 
-module.exports = class DenaliTypescriptBlueprint extends Blueprint {
+class DenaliTypescriptBlueprint extends Blueprint {}
 
-  static blueprintName = 'denali-typescript';
-  static description = 'Installs denali-typescript';
+DenaliTypescriptBlueprint.blueprintName = 'denali-typescript';
+DenaliTypescriptBlueprint.description = 'Installs denali-typescript';
 
-}
+module.exports = DenaliTypescriptBlueprint;
