817ef0cc682a8cec1493ec433c38a594a4841f96
Dave Wasmer <davewasmer@gmail.com>
2018-02-09 23:36:38 -0700

    test: add acceptance test

---
+++ test/acceptance/transpiles-with-typescript.ts
@@ -0,0 +1,20 @@
+import test from 'ava';
+import * as fs from 'fs';
+import * as path from 'path';
+import { CommandAcceptanceTest } from 'denali-cli';
+
+test('transpiles source with typescript', async (t) => {
+  let build = new CommandAcceptanceTest('build', { name: 'build-command' });
+  await build.run({ failOnStderr: true });
+  let builtFile = path.join(build.dir, 'dist', 'app', 'application.js');
+
+  t.true(fs.existsSync(builtFile), 'built files were created');
+  t.false(fs.readFileSync(builtFile, 'utf-8').includes('import'), 'built files were transpiled');
+});
+
+test.failing('checks code with tslint', async (t) => {
+  let build = new CommandAcceptanceTest('build', { name: 'build-command' });
+  let { output } = await build.run();
+
+  t.true(output.includes('foo'));
+});
